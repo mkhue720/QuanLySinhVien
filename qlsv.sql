ae4586a8d42701ed8cda6ffec6b41fe369ab87d1
NMK <mkhue720@gmail.com>
2025-02-09 18:10:51 +0700

    add

---
+++ qlsv.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test`.`student` (
+  `msv` VARCHAR(50) NOT NULL,
+  `ten` NVARCHAR(45) NOT NULL,
+  `lop` VARCHAR(45) NOT NULL,
+  `gpa` Double NOT NULL,
+  PRIMARY KEY (`msv`));
+
+SELECT * FROM test.student;
